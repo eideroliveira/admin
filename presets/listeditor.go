ee7761e2aa015e6e09450c92590d583d107b6bb0
Eider Oliveira <eider.oliveira@gmail.com>
2025-11-12 22:02:58 -0300

    Refactor ListEditor VSlot parameters and Generating the full iframe HTML, to force vuetify to render the updated content.

--- presets/listeditor.go
+++ presets/listeditor.go
@@ -263,8 +263,8 @@ func (b *ListEditorBuilder) MarshalHTML(c context.Context) (r []byte, err error)
    				).Attr("v-show", h.JSONString(!isSortStart)).
 					Class("mt-1 mb-4"),
 			),
-		).Init(h.JSONString(sorterData)).VSlot("{ locals }"),
-
+		).Init(h.JSONString(sorterData)).VSlot("{ locals, dash }").
+			DashInit("{errorMessages:{},disabled:{}}"),
 		// Read-only view when user doesn't have update permission or field is disabled
 		h.If(b.fieldContext.Disabled || !hasUpdatePermission,
 			h.Div(
