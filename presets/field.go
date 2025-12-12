0f0e284e8acc3861d05bb65d6666f35a224881e8
Eider Oliveira <eider.oliveira@gmail.com>
2025-12-12 09:47:48 -0300

    feat: add GetLabel method to FieldBuilder

--- presets/field.go
+++ presets/field.go
@@ -159,6 +159,10 @@ func (b *FieldBuilder) Label(v string) (r *FieldBuilder) {
 	return b
 }
 
+func (b *FieldBuilder) GetLabel() string {
+	return b.label
+}
+
 func (b *FieldBuilder) Clone() (r *FieldBuilder) {
 	r = &FieldBuilder{}
 	r.name = b.name
