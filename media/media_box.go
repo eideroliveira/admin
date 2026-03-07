440b1ca999081a3c64fe30601a3a5cbf441c769e
Eider Oliveira <eider.oliveira@gmail.com>
2026-03-07 11:19:05 -0300

    feat: Synchronize tab selection with URL hash and safely extract file extensions for media icons.

--- media/media_box.go
+++ media/media_box.go
@@ -242,8 +242,12 @@ func mediaBoxThumb(msgr *Messages, cfg *media_library.MediaBoxConfig,
 }
 
 func fileThumb(filename string) h.HTMLComponent {
+	ext := path.Ext(filename)
+	if len(ext) > 0 {
+		ext = ext[1:]
+	}
 	return h.Div(
-		fileicons.Icon(path.Ext(filename)[1:]).Attr("height", cardTitleHeight).Class("pt-4"),
+		fileicons.Icon(ext).Attr("height", cardTitleHeight).Class("pt-4"),
 	).Class("d-flex align-center justify-center")
 }
 
--- presets/common_page.go
+++ presets/common_page.go
@@ -38,7 +38,11 @@ func defaultToPage(config commonPageConfig, obj interface{}, ctx *web.EventConte
 				VTabs(
 					VTab(h.Text(msgr.FormTitle)).Value("default"),
 					h.Components(tabs...),
-				).Class("v-tabs--fixed-tabs").Attr("v-model", "locals.tab"),
+				).Class("v-tabs--fixed-tabs").Attr("v-model", "locals.tab").
+					Attr("@update:model-value", `function(v) {
+						if (v === 'default') { history.replaceState(null, '', location.pathname + location.search) }
+						else { location.hash = v }
+					}`),
 
 				VTabsWindow(
 					VTabsWindowItem(
@@ -46,7 +50,7 @@ func defaultToPage(config commonPageConfig, obj interface{}, ctx *web.EventConte
 					).Value("default"),
 					h.Components(contents...),
 				).Attr("v-model", "locals.tab"),
-			).VSlot("{ locals }").Init(`{tab: 'default'}`)
+			).VSlot("{ locals }").Init(`{tab: (location.hash ? location.hash.slice(1) : 'default')}`)
 		}
 	}
 
